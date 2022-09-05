861178136385c6243aea318f4dc09017674470d8
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2022-09-05 19:22:30 +0700

    fix review page overflow

--- lib/modules/review/review_restaurant.dart
+++ lib/modules/review/review_restaurant.dart
@@ -17,87 +17,89 @@ class ReviewRestaurant extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () => Get.back(),
-                    icon: Container(
-                      padding: const EdgeInsets.all(4.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: Colors.grey.shade200,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () => Get.back(),
+                      icon: Container(
+                        padding: const EdgeInsets.all(4.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.grey.shade200,
+                        ),
+                        child: const Icon(Icons.arrow_back),
                       ),
-                      child: const Icon(Icons.arrow_back),
                     ),
-                  ),
-                  Text('add_review'.tr, style: kHeadingRegular),
-                  IconButton(
-                    onPressed: () {
-                      if (controller.nameController.text.isNotEmpty &&
-                          controller.reviewController.text.isNotEmpty) {
-                        final review = ReviewModel(
-                          id: id,
-                          name: controller.nameController.text,
-                          review: controller.reviewController.text,
-                        );
-                        controller.addReview(review.toJson(), id);
-                      } else {
-                        Get.snackbar(
-                          'error'.tr,
-                          'fill_required'.tr,
-                        );
-                      }
-                    },
-                    icon: const Icon(Icons.send),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 24.0),
-              TextField(
-                controller: controller.nameController,
-                style: kBodyRegular,
-                decoration: InputDecoration(
-                  isDense: true,
-                  hintText: 'name'.tr,
-                  hintStyle: kBodyRegularHint,
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: kAmber, width: 1.0),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey.shade200,
+                    Text('add_review'.tr, style: kHeadingRegular),
+                    IconButton(
+                      onPressed: () {
+                        if (controller.nameController.text.isNotEmpty &&
+                            controller.reviewController.text.isNotEmpty) {
+                          final review = ReviewModel(
+                            id: id,
+                            name: controller.nameController.text,
+                            review: controller.reviewController.text,
+                          );
+                          controller.addReview(review.toJson(), id);
+                        } else {
+                          Get.snackbar(
+                            'error'.tr,
+                            'fill_required'.tr,
+                          );
+                        }
+                      },
+                      icon: const Icon(Icons.send),
+                    ),
+                  ],
                 ),
-              ),
-              const SizedBox(height: 10.0),
-              TextField(
-                controller: controller.reviewController,
-                style: kBodyRegular,
-                keyboardType: TextInputType.multiline,
-                maxLines: 5,
-                decoration: InputDecoration(
-                  isDense: true,
-                  hintText: 'review'.tr,
-                  hintStyle: kBodyRegularHint,
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    borderSide: BorderSide.none,
+                const SizedBox(height: 24.0),
+                TextField(
+                  controller: controller.nameController,
+                  style: kBodyRegular,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    hintText: 'name'.tr,
+                    hintStyle: kBodyRegularHint,
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: kAmber, width: 1.0),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey.shade200,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: kAmber, width: 1.0),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey.shade200,
                 ),
-              ),
-            ],
+                const SizedBox(height: 10.0),
+                TextField(
+                  controller: controller.reviewController,
+                  style: kBodyRegular,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    hintText: 'review'.tr,
+                    hintStyle: kBodyRegularHint,
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: kAmber, width: 1.0),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey.shade200,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
